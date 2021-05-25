2ee3b9b3abe239464bffb3f0a26afc235c6ee1bd
hffriman <henfriman.second@gmail.com>
2021-05-25 10:22:07 +0300

    Added loop exercise for Day02

--- Day02/instruments.py
+++ Day02/instruments.py
@@ -2,11 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-name = "Henry"
-
 @app.route("/")
 def index():
-	return render_template("base.html")
+	instruments=["Piano", "Violin", "Guitar"]
+	return render_template("base.html", instruments=instruments)
 
 if __name__ == "__main__":
 	app.run()
