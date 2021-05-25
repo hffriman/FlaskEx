a72b20488a9ee16487a27a39afe7d2c6c18bb51d
hffriman <henfriman.second@gmail.com>
2021-05-25 16:02:19 +0300

    Added calculator exercise

--- Day02/templated.py
+++ Day02/templated.py
@@ -4,7 +4,7 @@
 # !!  problems during class: it helped me to fix my
 # !!  own problem in rendering the "music" array
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -19,9 +19,14 @@ def extension():
 	music = ["Classic", "Pop", "Rock", "Rap"]
 	return render_template("extension.html", music=music)
 
-@app.route("/postform")
+@app.route("/postform", methods=["GET", "POST"])
 def postform():
-	return render_template("postform.html")
+	area = 0
+	if "side" in request.form:
+		side = int(request.form["side"])
+		area = side*side
+	return render_template("postform.html", area=area)
+
 
 @app.route("/getform")
 def getform():
