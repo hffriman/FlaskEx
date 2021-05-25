208329fdf301efb435645537c88fd3eddab09835
hffriman <henfriman.second@gmail.com>
2021-05-25 14:10:34 +0300

    Added Get and Form templates

--- Day02/templated.py
+++ Day02/templated.py
@@ -1,3 +1,9 @@
+# Sources:
+# -- Course material
+# !!  Special thanks to co-students for sharing their
+# !!  problems during class: it helped me to fix my
+# !!  own problem in rendering the "music" array
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +13,19 @@ name = "Henry"
 @app.route("/")
 def index():
 	return render_template("base.html")
+
+@app.route("/extension")
+def extension():
+	music = ["Classic", "Pop", "Rock", "Rap"]
+	return render_template("extension.html", music=music)
+
+@app.route("/postform")
+def postform():
+	return render_template("postform.html")
+
+@app.route("/getform")
+def getform():
+	return render_template("getform.html")
 
 if __name__ == "__main__":
 	app.run()
