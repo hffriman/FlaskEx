a09e3f5617edd12f173bc9b5ce3eefb283ed8a6b
hffriman <henfriman.second@gmail.com>
2021-05-25 22:25:28 +0300

    Updated my homework in folder pw02

--- Homework/pw02/pw2.3/repeat.py
+++ Homework/pw02/pw2.3/repeat.py
@@ -1,3 +1,5 @@
+#Sources: course material
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
--- Homework/pw02/pw2.4/inputform.py
+++ Homework/pw02/pw2.4/inputform.py
@@ -1,3 +1,5 @@
+#Sources: course material
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
